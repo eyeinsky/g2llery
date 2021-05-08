469b11944d473eb9b285e576c7063600404d64f0
Markus Läll <markus.l2ll@gmail.com>
2021-05-09 02:13:20 +0300

    Browse root folder

--- src/Main.hs
+++ src/Main.hs
@@ -3,11 +3,31 @@ module Main where
 import qualified Network.Wai.Handler.Warp as Warp
 import qualified Rapid
 import System.IO
+import System.Directory
+
+import Options.Applicative as O
 
 import X.Prelude as P
-import X
+import X hiding (path, contents)
+import X.Template.V2
+import URL
 import URL.TH
 import Warp_Helpers (tlsSettingsEnvIO)
+
+-- * CLI
+
+data Options = Options
+  { rootPath :: FilePath
+  , verbose :: Bool
+  } deriving (Show)
+
+opts :: Parser Options
+opts = Options <$> path' <*> pure False
+  where
+    path' = O.argument O.str
+      $ metavar "FILE"
+      <> help "Gallery root path"
+      <> value "."
 
 -- * Site
 
@@ -17,18 +37,34 @@ makeFields ''RunConf
 instance Default RunConf where
   def = RunConf mempty
 
-site :: T RunConf
-site = T $ do
+data FolderListing = FolderListing
+  { folderListingPath :: FilePath
+  , folderListingContents :: [FilePath]
+  }
+makeFields ''FolderListing
+
+getFolderListing :: FilePath -> IO FolderListing
+getFolderListing path = do
+  list <- listDirectory path
+  return $ FolderListing path list
+
+site :: Options -> T RunConf
+site opts = T $ do
   return $ \_ -> mdo
-    return $ htmlDoc "" "hello world"
+    fl <- liftIO $ getFolderListing $ rootPath opts
+    return $ htmlDoc "" $ do
+      h1 $ fl^.path.html
+      ul $ forM_ (fl^.contents) $ do
+        li . toHtml
 
 -- * Web server
 
 main :: IO ()
 main = do
   hSetBuffering stdout LineBuffering
+  opts :: Options <- execParser (info opts idm)
   maybeTls <- tlsSettingsEnvIO "DEV_WEBSERVER_CERT" "DEV_WEBSERVER_KEY"
-  siteMain maybeTls def def prodUrl settings site
+  siteMain maybeTls def def prodUrl settings $ site opts
   where settings = Warp.setPort 8082 Warp.defaultSettings
 
 prodUrl :: URL
