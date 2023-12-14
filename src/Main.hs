e25ec4a119386b2428eacab3e491250df4f7f22a
Markus Läll <markus.l2ll@gmail.com>
2023-12-14 22:16:06 +0200

    Get compiling via cabal

--- src/Main.hs
+++ src/Main.hs
@@ -1,79 +1,87 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE FunctionalDependencies #-}
+
 module Main where
 
-import qualified Network.Wai.Handler.Warp as Warp
-import qualified Rapid
+import Network.Wai.Handler.Warp qualified as Warp
+import Rapid qualified
 import System.IO
 import System.Directory
 
-import Options.Applicative as O
+import Options.Applicative qualified as O
 
-import X.Prelude as P
-import X hiding (path, contents)
-import X.Template.V2
+import Common.Prelude as P
+-- import X hiding (path, contents)
+-- import X.Template.V2
 import URL
 import URL.TH
-import Warp_Helpers (tlsSettingsEnvIO)
+-- import Warp_Helpers (tlsSettingsEnvIO)
+
+import Server.Wai
+
+import Servant.Server
+import Servant
 
 -- * CLI
 
 data Options = Options
   { rootPath :: FilePath
+  , url :: String
   , verbose :: Bool
   } deriving (Show)
 
-opts :: Parser Options
-opts = Options <$> path' <*> pure False
-  where
-    path' = O.argument O.str
-      $ metavar "FILE"
-      <> help "Gallery root path"
-      <> value "."
+opts :: O.Parser Options
+opts = Options
+  <$> O.argument O.str (O.metavar "FILE" <> O.help "Gallery root path" <> O.value ".")
+  <*> O.strOption (O.long "url")
+  <*> pure False
 
 -- * Site
 
-data RunConf = RunConf { runConfDynPath :: [Segment] }
-makeFields ''RunConf
+main :: IO ()
+main = do
+  opts :: Options <- O.execParser (O.info opts O.idm)
+  maybeTls <- tlsSettingsEnv "DEV_WEBSERVER_CERT" "DEV_WEBSERVER_KEY"
+  return ()
+--  siteMain maybeTls def def prodUrl settings $ site opts
+  where settings = Warp.setPort 8082 Warp.defaultSettings
 
-instance Default RunConf where
-  def = RunConf mempty
+-- data RunConf = RunConf { runConfDynPath :: [Segment] }
+-- makeFields ''RunConf
 
-data FolderListing = FolderListing
-  { folderListingPath :: FilePath
-  , folderListingContents :: [FilePath]
-  }
-makeFields ''FolderListing
+-- instance Default RunConf where
+--   def = RunConf mempty
 
-getFolderListing :: FilePath -> IO FolderListing
-getFolderListing path = do
-  list <- listDirectory path
-  return $ FolderListing path list
+-- data FolderListing = FolderListing
+--   { folderListingPath :: FilePath
+--   , folderListingContents :: [FilePath]
+--   }
+-- makeFields ''FolderListing
 
-site :: Options -> T RunConf
-site opts = T $ do
-  return $ \_ -> mdo
-    fl <- liftIO $ getFolderListing $ rootPath opts
-    return $ htmlDoc "" $ do
-      h1 $ fl^.path.html
-      ul $ forM_ (fl^.contents) $ do
-        li . toHtml
+-- getFolderListing :: FilePath -> IO FolderListing
+-- getFolderListing path = do
+--   list <- listDirectory path
+--   return $ FolderListing path list
+
+-- site :: Options -> T RunConf
+-- site opts = T $ do
+--   return $ \_ -> do
+--     fl <- liftIO $ getFolderListing $ rootPath opts
+--     return $ htmlDoc "" $ do
+--       h1 $ fl^.path.html
+--       ul $ forM_ (fl^.contents) $ do
+--         li . toHtml
 
 -- * Web server
 
-main :: IO ()
-main = do
-  hSetBuffering stdout LineBuffering
-  opts :: Options <- execParser (info opts idm)
-  maybeTls <- tlsSettingsEnvIO "DEV_WEBSERVER_CERT" "DEV_WEBSERVER_KEY"
-  siteMain maybeTls def def prodUrl settings $ site opts
-  where settings = Warp.setPort 8082 Warp.defaultSettings
+-- prodUrl :: URL
+-- prodUrl = [url| https://fixme.com/ |]
 
-prodUrl :: URL
-prodUrl = [url| https://fixme.com/ |]
+-- -- * Hot reload
 
--- * Hot reload
-
-hot, stop :: IO ()
-hot = Rapid.rapid 0 $ \r -> do
-  Rapid.restart r ("webserver" :: String) main
-stop = Rapid.rapid 0 $ \r -> do
-  Rapid.stop r ("webserver" :: String) main
+-- hot, stop :: IO ()
+-- hot = Rapid.rapid 0 $ \r -> do
+--   Rapid.restart r ("webserver" :: String) main
+-- stop = Rapid.rapid 0 $ \r -> do
+--   Rapid.stop r ("webserver" :: String) main
